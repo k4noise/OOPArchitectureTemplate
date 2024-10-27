3e7a1d830be258f453a03f0cfb5789d08d255bbc
k4noise <vestertp@gmail.com>
2024-10-27 22:53:49 +0500

    fix: add missed javadoc in test class

--- src/test/java/ru/urfu/MessageProcessorTest.java
+++ src/test/java/ru/urfu/MessageProcessorTest.java
@@ -3,8 +3,14 @@ package ru.urfu;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Тесты к обработчику пользовательских сообщений
+ */
 class MessageProcessorTest {
 
+    /**
+     * Проверяет, корректно ли подставляется метка к сообщению пользователя для его повторения
+     */
     @Test
     void testGetRepeatable() {
         String message = "Привет";
